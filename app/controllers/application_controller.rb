a083ef1afd59072a689feb8daee6e91e38997ad8
EC2 Default User <ec2-user@ip-172-31-6-194.ap-northeast-1.compute.internal>
2023-07-20 06:38:23 +0000

    [Add]admin_session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,19 @@
 class ApplicationController < ActionController::Base
   private
-  #def after_sign_in_path_for(resource_or_scope)
-    #if resource_or_scope.is_a?(Admin)
-        #admins_orders_top_path
-    #else
-        #customers_mypage_path
-    #end
-  #end
+  def after_sign_in_path_for(resource_or_scope)
+    if resource_or_scope.is_a?(Admin)
+        admin_top_path
+    else
+        customers_mypage_path
+    end
+  end
 
-  #def
-    #after_sign_out_path_for(resource_or_scope)
-    #if resource_or_scope == :user
-       # homes_about_path
-    #elsif resource_or_scope == :admin
-    #homes_about_path
-       #end
+  def
+    after_sign_out_path_for(resource_or_scope)
+    if resource_or_scope == :customer
+        homes_about_path
+    elsif resource_or_scope == :admin
+          new_admin_session_path
+    end
+end
 end
