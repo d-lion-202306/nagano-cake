14b6dd12488a048468e72077aac39c3ce59a2c6d
EC2 Default User <ec2-user@ip-172-31-35-78.ap-northeast-1.compute.internal>
2023-07-20 16:26:04 +0900

    [update]devise_customer

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,10 +1,37 @@
 class Public::CustomersController < ApplicationController
+  before_action :authenticate_customer!
   def show
+    @customer = Customer.find(current_customer.id)
   end
 
   def edit
+    @customer = Customer.find(current_customer.id)
   end
-
+  
+  def update
+    @customer = Customer.find(current_customer.id)
+    if @customer.update(customer_params)
+       flash[:notice] ="You have updated successfully."
+       redirect_to customers_mypage_path
+    else
+      render :edit
+    end
+  end
+  
   def confirm_withdraw
   end
+  
+  def withdraw
+    @customer = Customer.find(current_customer.id)
+    @customer.update(is_deleted: true)
+    reset_session
+    redirect_to root_path
+  end
+  
+  private
+
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :post_code, :address, :telephone_number, :email)
+  end
+  
 end
